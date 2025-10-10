d47eb13426555250595c3bb20ddc58dce5026186
Pratyushaa94 <prathyusha9449@gmail.com>
2025-10-10 11:49:32 +0000

    changes

--- terraform/environments/dev/key-pair/outputs.tf
+++ terraform/environments/dev/key-pair/outputs.tf
@@ -1,16 +1,13 @@
 # =============================================================================
 # OUTPUTS - DEV ENVIRONMENT (KEYPAIR MODULE)
 # =============================================================================
-# These outputs display the generated AWS key pair name and location of
-# the saved private key file.
-# =============================================================================
 
 output "keypair_name" {
   description = "AWS Key Pair Name created for dev environment"
-  value       = module.key_pair.key_name
+  value       = module.key_pair.keypair_name
 }
 
-output "keypair_private_key_path" {
+output "private_key_path" {
   description = "Local path of the generated PEM file"
-  value       = module.key_pair.keypair_private_key_path
+  value       = module.key_pair.private_key_path
 }
